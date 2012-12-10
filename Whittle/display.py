6fcce85759cfffa4a081b256d0441e90ac4807b6
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-12-10 14:51:31 +1300

    disply

---
+++ Whittle/display.py
@@ -0,0 +1,28 @@
+import numpy as np
+import numpy.random as rng
+import matplotlib.pyplot as plt
+
+samples = np.atleast_2d(np.loadtxt('posterior_sample.txt'))
+data = np.loadtxt('simulated_data.txt')
+maxNumComponents = (samples.shape[1] - 3)//3
+
+for i in xrange(0, samples.shape[0]):
+	model = samples[i, 3:]
+
+	A = model[0:maxNumComponents]
+	f = model[maxNumComponents:2*maxNumComponents]
+	w = model[2*maxNumComponents:]
+	which = A > 0.
+	A, f, w = A[which], f[which], w[which]
+
+	y = np.zeros(data.shape[0])
+	for j in xrange(0, A.size):
+		y += A[j]/(1. + ((data[:,0] - f[j])/w[j])**2)
+
+	plt.figure(figsize=(12, 6))
+	plt.clf()
+	plt.hold(True)
+	plt.plot(data[:,0], data[:,1], 'b')
+	plt.plot(data[:,0], y, 'r')
+	plt.show()
+
